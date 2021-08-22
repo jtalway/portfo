d354e5a90974428dc9d3e4be2700673a4fad950f
jatlway <jtalway@gmail.com>
2021-08-22 08:47:18 -0500

    change openfile file path

--- openfile.py
+++ openfile.py
@@ -1,6 +1,6 @@
 def openfile(f):
 # File Read Function
-    with open('./static/assets/'+ f +'.txt') as fp:
+    with open('/home/jtalway/portfo/static/assets/'+ f +'.txt') as fp:
         f_array = []
         f_array = fp.readlines()
         fp.close()
