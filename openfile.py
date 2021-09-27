aa4e8f6cafd42db2636165fa300f69807587b3e6
Jason Alway <jtalway@gmail.com>
2021-09-27 16:37:08 -0500

    openfile path

--- openfile.py
+++ openfile.py
@@ -1,7 +1,7 @@
 def openfile(f):
 # File Read Function
-    with open('./static/assets/'+ f +'.txt') as fp:
-    #with open('/home/jtalway/portfo/static/assets/'+ f +'.txt') as fp:
+    #with open('./static/assets/'+ f +'.txt') as fp:
+    with open('/home/jtalway/portfo/static/assets/'+ f +'.txt') as fp:
         f_array = []
         f_array = fp.readlines()
         fp.close()
