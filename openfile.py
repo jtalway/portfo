ca02f1da2f39f0c8a5822f3e9938465bf29f9bab
Jason Alway <jtalway@gmail.com>
2022-07-01 21:37:42 -0500

    filepath

--- openfile.py
+++ openfile.py
@@ -3,8 +3,8 @@ from os import lseek
 
 def openfile(f):
 # File Read Function
-    with open('./static/assets/'+ f +'.txt') as fp:
-    #with open('/home/jtalway/portfo/static/assets/'+ f +'.txt') as fp:
+    #with open('./static/assets/'+ f +'.txt') as fp:
+    with open('/home/jtalway/portfo/static/assets/'+ f +'.txt') as fp:
         f_array = []
         f_array = fp.readlines()
         fp.close()
